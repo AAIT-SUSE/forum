b40f54dc61280ae140ff86549c45b6262b723fee
Hooyan <42812042+Hooyan@users.noreply.github.com>
2018-11-10 17:35:26 +0800

    found  home page
    found  home page

---
+++ src/views/Found.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container grid-list-md align-content-center>
+    <v-flex>
+      <div style="margin-top:45px" >
+        <FoundSearch></FoundSearch>
+      </div>
+    </v-flex>
+  </v-container>
+</template>
+
+
+
+<script>
+import FoundSearch from './components/FoundSearch.vue'
+
+export default {
+  name:'Found',
+  components:{
+    FoundSearch,
+  }
+ 
+}
+
+</script>
+
+
+<style scoped>
+
+
+</style>
