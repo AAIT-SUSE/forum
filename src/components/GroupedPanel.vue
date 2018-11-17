5b917d73e649b72b66b699b29d95eff56278516e
Hooyan <42812042+Hooyan@users.noreply.github.com>
2018-11-17 21:58:37 +0800

    Update GroupedPanel.vue

--- src/components/GroupedPanel.vue
+++ src/components/GroupedPanel.vue
@@ -1,63 +1,52 @@
 <template>
   <v-card>
-    <v-toolbar color="light-blue" dark>
-      <v-toolbar-title>{{ topTitle }}</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon @click="ShowSearchInput()">
-        <v-icon>search</v-icon>
-      </v-btn>
+    <v-toolbar dense color="light-blue" dark prominent tabs>
+      <v-flex lg11>
+        <v-tabs slot="extension" v-model="tab" left color="transparent" slider-color="red">
+          <v-tab v-for="item in items" :key="item.name">
+            {{ item.name }}
+          </v-tab>
+        </v-tabs>
+      </v-flex>
+      <v-flex lg1>
+        <v-btn icon @click="ShowSearchInput()">
+         <v-icon>search</v-icon>
+       </v-btn>
+      </v-flex>
     </v-toolbar>
-
-    <v-list two-line subheader>
-      <div v-for="group in groups" :key="group.id">
-        <v-subheader inset>
-          {{ group.name }}
-        </v-subheader>
-
-        <v-list-tile
-          v-for="item in group.items"
-          :key="item.id"
-          @click=";"
-          avatar
-        >
-          <v-list-tile-avatar>
-            <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-
-          <v-list-tile-action>
-            <v-btn icon ripple>
-              <v-icon color="grey lighten-1">keyboard_arrow_right</v-icon>
-            </v-btn>
-          </v-list-tile-action>
-        </v-list-tile>
-
-        <v-divider inset v-if="groups.indexOf(group) < groups.length - 1"></v-divider>
-      </div>
-    </v-list> 
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item.name">
+        <v-card flat>
+          <GroupList v-if="item.value === 'GroupList'"></GroupList>
+          <ForumList v-if="item.value === 'ForumList'"></ForumList>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 
 <script>
+import GroupList from '../components/GroupList.vue'
+import ForumList from '../components/ForumList.vue'
+
 export default {
-  props: {
-    topTitle: String,
-    groups: {
-      id: Number,
-      name: String,
-      items: {
-        id: Number,
-        title: String,
-        subtitle: String,
-        iconClass: String,
-        icon: String,
-      }
+  components:{
+    GroupList,
+    ForumList,
+  },
+  data() {
+    return{
+      tab:null,
+      items:[
+        {
+          name:"小组",
+          value:'GroupList'
+        },
+        {
+          name:"论坛",
+          value:'ForumList'
+        }
+      ]
     }
   },
 }
