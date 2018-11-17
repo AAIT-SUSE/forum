0ea313e782ac58ebd3034d9462c7163a5f3cc690
Hooyan <42812042+Hooyan@users.noreply.github.com>
2018-11-17 21:57:12 +0800

    删除groups值

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -52,26 +52,6 @@ export default {
           color: 'success'
         },
       ],
-      topTitle: '论坛',
-      groups: [
-        {
-          id: 0,
-          name: '公告 / 新闻',
-          items: [
-            {id: 0, title: '公告', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'announcement'},
-            {id: 1, title: '活动与新闻', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'local_activity'},
-          ]
-        },
-        {
-          id: 1,
-          name: '技术交流',
-          items: [
-            {id: 3, title: 'C / C++', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'code'},
-            {id: 4, title: 'Unity 游戏开发', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'videogame_asset'},
-            {id: 5, title: 'Web 开发', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'web'},
-          ]
-        },
-      ]
     }
   },
 }
