c77be8df2d33a4a47fd995ed6b122bd05958a440
Owen Tsai <owentsai.v@gmail.com>
2018-11-20 20:50:35 +0800

    Update login form background

--- src/components/LoginAndRegister.vue
+++ src/components/LoginAndRegister.vue
@@ -1,134 +1,134 @@
 <template>
   <div id="loginWindow" v-if="!isUserLogged">
-  <v-card
-    class="mx-auto"
-    width="550px"
-    raised
-    dark
-  >
-    <v-card-title class="title font-weight-regular justify-space-between">
-      <span>{{ currentTitle }}</span>
-      <v-avatar
-        color="primary lighten-2"
-        class="subheading white--text"
-        size="34"
-        v-text="step + '/5'"
-      ></v-avatar>
-    </v-card-title>
+    <v-card
+      class="mx-auto"
+      width="550px"
+      raised
+      dark
+    >
+      <v-card-title class="title font-weight-regular justify-space-between">
+        <span>{{ currentTitle }}</span>
+        <v-avatar
+          color="primary lighten-2"
+          class="subheading white--text"
+          size="34"
+          v-text="step + '/5'"
+        ></v-avatar>
+      </v-card-title>
 
-    <v-window v-model="step">
-      <v-window-item :value="1">
-        <v-card-text>
-          <v-text-field
-            label="你的邮箱"
-            placeholder="example@gmail.com"
-            v-model="email"
-            :rules="[rules.required, rules.email]"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            你所填写的邮箱将作为您登录 Envision 所需要的账户名称。
-          </span>
-        </v-card-text>
-      </v-window-item>
+      <v-window v-model="step">
+        <v-window-item :value="1">
+          <v-card-text>
+            <v-text-field
+              label="你的邮箱"
+              placeholder="example@gmail.com"
+              v-model="email"
+              :rules="[rules.required, rules.email]"
+            ></v-text-field>
+            <span class="caption grey--text text--darken-1">
+              你所填写的邮箱将作为您登录 Envision 所需要的账户名称。
+            </span>
+          </v-card-text>
+        </v-window-item>
 
-      <v-window-item :value="2">
-        <v-card-text>
-          <v-text-field
-            label="密码"
-            v-model="password"
-            type="password"
-          ></v-text-field>
-          <v-text-field
-            label="确认密码"
-            v-model="confirmPassword"
-            type="password"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            为你的账户创建一个密码。你需要使用此密码进行登录，请妥善保管。
-          </span>
-        </v-card-text>
-      </v-window-item>
+        <v-window-item :value="2">
+          <v-card-text>
+            <v-text-field
+              label="密码"
+              v-model="password"
+              type="password"
+            ></v-text-field>
+            <v-text-field
+              label="确认密码"
+              v-model="confirmPassword"
+              type="password"
+            ></v-text-field>
+            <span class="caption grey--text text--darken-1">
+              为你的账户创建一个密码。你需要使用此密码进行登录，请妥善保管。
+            </span>
+          </v-card-text>
+        </v-window-item>
 
-      <v-window-item :value="3">
-        <v-card-text>
-          <v-text-field
-            label="真实姓名"
-          ></v-text-field>
-          <v-text-field
-            label="专业"
-          ></v-text-field>
-          <v-layout grid-list-md>
-            <v-flex xs12 sm12 md6 lg6 xl6 class="mr-1">
-              <v-select
-                :items="years"
-                label="年级"
-              ></v-select>
-            </v-flex>
-            <v-flex xs12 sm12 md6 lg6 xl6 class="ml-1">
-              <v-select
-                :items="classes"
-                :disabled="isTeacher"
-                label="班级"
-              ></v-select>
-            </v-flex>
-          </v-layout>
-          <v-select
-            :items="depts"
-            label="所属部门"
-          ></v-select>
-          <span class="caption grey--text text--darken-1">
-            为了验证你的身份，你需要填写以上信息。您稍后可以选择是否在您的个人面板中展示这些信息
-          </span>
-        </v-card-text>
-      </v-window-item>
+        <v-window-item :value="3">
+          <v-card-text>
+            <v-text-field
+              label="真实姓名"
+            ></v-text-field>
+            <v-text-field
+              label="专业"
+            ></v-text-field>
+            <v-layout grid-list-md>
+              <v-flex xs12 sm12 md6 lg6 xl6 class="mr-1">
+                <v-select
+                  :items="years"
+                  label="年级"
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm12 md6 lg6 xl6 class="ml-1">
+                <v-select
+                  :items="classes"
+                  :disabled="isTeacher"
+                  label="班级"
+                ></v-select>
+              </v-flex>
+            </v-layout>
+            <v-select
+              :items="depts"
+              label="所属部门"
+            ></v-select>
+            <span class="caption grey--text text--darken-1">
+              为了验证你的身份，你需要填写以上信息。您稍后可以选择是否在您的个人面板中展示这些信息
+            </span>
+          </v-card-text>
+        </v-window-item>
 
-      <v-window-item :value="4">
-        <v-card-text>
-          <v-text-field
-            label="Password"
-            type="password"
-          ></v-text-field>
-          <v-text-field
-            label="Confirm Password"
-            type="password"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            为你的账户创建一个密码。你需要使用此密码进行登录，请妥善保管。
-          </span>
-        </v-card-text>
-      </v-window-item>
+        <v-window-item :value="4">
+          <v-card-text>
+            <v-text-field
+              label="Password"
+              type="password"
+            ></v-text-field>
+            <v-text-field
+              label="Confirm Password"
+              type="password"
+            ></v-text-field>
+            <span class="caption grey--text text--darken-1">
+              为你的账户创建一个密码。你需要使用此密码进行登录，请妥善保管。
+            </span>
+          </v-card-text>
+        </v-window-item>
 
 
-      <v-window-item :value="5">
-        <div class="pa-3 text-xs-center">
-          <h3 class="title font-weight-light mb-2">感谢您使用 Envision</h3>
-          <span class="caption grey--text">您的账号已经创建成功，点击完成按钮载入应用</span>
-        </div>
-      </v-window-item>
-    </v-window>
+        <v-window-item :value="5">
+          <div class="pa-3 text-xs-center">
+            <h3 class="title font-weight-light mb-2">感谢您使用 Envision</h3>
+            <span class="caption grey--text">您的账号已经创建成功，点击完成按钮载入应用</span>
+          </div>
+        </v-window-item>
+      </v-window>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-btn
-        :disabled="step === 1"
-        flat
-        @click="step--"
-      >
-        上一步
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        :disabled="!nextStepOk"
-        color="primary"
-        depressed
-        @click="NextStep()"
-      >
-        {{nextBtnText}}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</div>
+      <v-card-actions>
+        <v-btn
+          :disabled="step === 1"
+          flat
+          @click="step--"
+        >
+          上一步
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          :disabled="!nextStepOk"
+          color="primary"
+          depressed
+          @click="NextStep()"
+        >
+          {{nextBtnText}}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
