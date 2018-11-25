88d450e74ded2d6709acc0a9e3b3f7b434d12c21
Hooyan <42812042+Hooyan@users.noreply.github.com>
2018-11-25 10:29:55 +0800

    重写论坛分区页面

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -1,16 +1,31 @@
 <template>
-  <v-container grid-list-md align-content-center>
-    <GroupedPanelList :groups='groups'></GroupedPanelList>
-  </v-container>
+  <v-list two-line subheader>
+    <div v-for="group in groups" :key="group.id">
+      <v-subheader inset>
+        {{ group.name }}
+      </v-subheader>
+      <v-list-tile v-for="item in group.items" :key="item.id" @click=";" avatar>
+        <v-list-tile-avatar>
+          <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">keyboard_arrow_right</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-divider inset v-if="groups.indexOf(group) < groups.length - 1"></v-divider>
+    </div>
+  </v-list>
 </template>
 
 <script>
-import GroupedPanelList from '../components/GroupedPanelList.vue'
 
 export default {
-    components:{
-        GroupedPanelList,
-    },
   data(){
     return {
       groups: [
