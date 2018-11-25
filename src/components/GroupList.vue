8a05e3967a1475a976ac1726fa6460e684c57d0a
Hooyan <42812042+Hooyan@users.noreply.github.com>
2018-11-25 10:31:08 +0800

    重写小组页面

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,39 +1,78 @@
 <template>
-  <v-container grid-list-md align-content-center>
-    <GroupedPanelList :groups='groups'></GroupedPanelList>
-  </v-container>
+  <v-list two-line subheader>
+    <div v-for="group in groups" :key="group.group_id">
+      <v-list-tile @click=";" avatar>
+        <v-list-tile-avatar>
+          <v-icon :class="[group.iconClass]">{{ group.icon }}</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ group.group_name }}
+            <font size="0.5px">|创建时间:{{group.creat_time}}</font>
+          </v-list-tile-title>
+          <v-list-tile-sub-title>{{ group.description }}</v-list-tile-sub-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">keyboard_arrow_right</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+    </div>
+  </v-list>
 </template>
 
 <script>
-import GroupedPanelList from '../components/GroupedPanelList.vue'
+import axios from 'axios'
 
 export default {
-    components:{
-      GroupedPanelList,
-    },
   data(){
     return {
       groups: [
         {
-          id: 0,
-          name: '公告 / 新闻',
-          items: [
-            {id: 0, title: '公告', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'announcement'},
-            {id: 1, title: '活动与新闻', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'local_activity'},
-          ]
+          group_id: 0,
+          group_name: 'C / C++',
+          description: '无最新消息',
+          iconClass: 'info white--text',
+          icon: 'code',
+          creat_time: '2018-11-25'
         },
         {
-          id: 1,
-          name: '技术交流',
-          items: [
-            {id: 3, title: 'C / C++', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'code'},
-            {id: 4, title: 'Unity 游戏开发', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'videogame_asset'},
-            {id: 5, title: 'Web 开发', subtitle: '无最新消息', iconClass: 'info white--text', icon: 'web'},
-          ]
+          group_id: 1,
+          group_name: 'Unity 游戏开发',
+          description: '无最新消息',
+          iconClass: 'info white--text',
+          icon: 'videogame_asset',
+          creat_time: '2018-11-24'
+        },
+        {
+          group_id: 2,
+          group_name: 'Web 开发',
+          description: '无最新消息',
+          iconClass: 'info white--text',
+          icon: 'web',
+          creat_time: '2018-11-23'
         },
       ]
     }
   },
+  // methods: {
+  //   GroupListGet: function() {
+  //     let self = this;
+  //     axios.get(`${'https://cors-anywhere.herokuapp.com/'}http://www.aait-suse.cn/api/GroupViewSet/`
+  //     ).
+  //     then(function(response) {
+  //       //console.log(response.data)
+  //       self.items = response.data;
+  //     }).
+  //     catch(function(error) {
+  //       console.log(error);
+  //     })
+  //   }
+  // },
+  // mounted() {
+  //   this.GroupListGet();
+  // }
 }
 </script>
 
